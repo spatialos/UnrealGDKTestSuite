e7adeadac5c0f47302f75e37aa5088aafae71bb8
INTERNAL\danielhall <danielhall@improbable.io>
2018-04-17 14:29:51 +0100

    updated scripts folder structure

--- create_spatialGDK_symlink.bat
+++ create_spatialGDK_symlink.bat
@@ -3,6 +3,7 @@
 set SPATIALGDK_PATH="%1"
 set SPATIALGDK_PLUGINSPATH="%1\Plugins\SpatialGDK"
 set SPATIALGDK_MODULEPATH="%1\SpatialGDK"
+set SPATIALGDK_SCRIPTSPATH="%1\Scripts"
 
 if %SPATIALGDK_PATH% == "" (
 	echo Error: Please specify the SpatialGDK path.
@@ -18,6 +19,7 @@ if not exist %SPATIALGDK_PATH%\ (
 set PATH_VALID=true
 if not exist %SPATIALGDK_PLUGINSPATH% set PATH_VALID=false
 if not exist %SPATIALGDK_MODULEPATH% set PATH_VALID=false
+if not exist %SPATIALGDK_SCRIPTSPATH% set PATH_VALID=false
 
 if %PATH_VALID%==false (
  	echo Error: SpatialGDK path %SPATIALGDK_PATH% is invalid. Provide path to cloned SpatialGDK git repository.
@@ -26,7 +28,8 @@ if %PATH_VALID%==false (
 
 REM Cleanup old symlinks
 rmdir "%~dp0\workers\unreal\Game\Plugins\SpatialGDK" 2>NUL
-rmdir "%~dp0\workers\unreal\Game\Source\SpatialGDK" 2>NUL
+rmdir "%~dp0\workers\unreal\Game\Source\SpatialGDK" 2> NUL
+rmdir "%~dp0\workers\unreal\Game\Scripts" 2> NUL
 
 REM Ensure plugins folder exists, mklink doesn't recursively create
 if not exist %~dp0\workers\unreal\Game\Plugins\ (
@@ -36,6 +39,7 @@ if not exist %~dp0\workers\unreal\Game\Plugins\ (
 REM Make new symlinks
 mklink /J "%~dp0\workers\unreal\Game\Plugins\SpatialGDK" %SPATIALGDK_PLUGINSPATH%
 mklink /J "%~dp0\workers\unreal\Game\Source\SpatialGDK" %SPATIALGDK_MODULEPATH%
+mklink /J "%~dp0\workers\unreal\Game\Scripts" %SPATIALGDK_SCRIPTSPATH%
 
 echo Successfully created symlinks to %SPATIALGDK_PATH%
 
