2c6133f4ffb304ab66a94d50d876983932afab56
INTERNAL\danielhall <danielhall@improbable.io>
2018-04-23 14:55:18 +0100

    Updated the symlinks paths

--- create_spatialGDK_symlink.bat
+++ create_spatialGDK_symlink.bat
@@ -4,7 +4,7 @@ set SPATIALGDK_PATH="%1"
 set SPATIALGDK_PLUGINSPATH="%1\Plugins\SpatialGDK"
 set SPATIALGDK_MODULEPATH="%1\Source\SpatialGDK"
 set SPATIALGDK_SCRIPTSPATH="%1\Scripts"
-set SPATIALGDK_CODEGENPATH="%1\CodeGeneration"
+set SPATIALGDK_CODEGENPATH="%1\Binaries\ThirdParty\Improbable"
 
 if %SPATIALGDK_PATH% == "" (
 	echo Error: Please specify the SpatialGDK path.
@@ -32,7 +32,7 @@ REM Cleanup old symlinks
 rmdir "%~dp0\workers\unreal\Game\Plugins\SpatialGDK" 2>NUL
 rmdir "%~dp0\workers\unreal\Game\Source\SpatialGDK" 2>NUL
 rmdir "%~dp0\workers\unreal\Game\Scripts" 2>NUL
-rmdir "%~dp0\workers\unreal\Game\Binaries\ThirdParty\Improbable\CodeGeneration" 2>NUL
+rmdir "%~dp0\workers\unreal\Game\Binaries\ThirdParty\Improbable" 2>NUL
 
 REM Ensure plugins folder exists, mklink doesn't recursively create
 if not exist %~dp0\workers\unreal\Game\Plugins\ (
@@ -40,15 +40,15 @@ if not exist %~dp0\workers\unreal\Game\Plugins\ (
 )
 
 REM Ensure codegen folder exists, mklink doesn't recursively create
-if not exist %~dp0\workers\unreal\Game\Binaries\ThirdParty\Improbable\ (
-	mkdir %~dp0\workers\unreal\Game\Binaries\ThirdParty\Improbable\
+if not exist %~dp0\workers\unreal\Game\Binaries\ThirdParty\ (
+	mkdir %~dp0\workers\unreal\Game\Binaries\ThirdParty\
 )
 
 REM Make new symlinks
 mklink /J "%~dp0\workers\unreal\Game\Plugins\SpatialGDK" %SPATIALGDK_PLUGINSPATH%
 mklink /J "%~dp0\workers\unreal\Game\Source\SpatialGDK" %SPATIALGDK_MODULEPATH%
 mklink /J "%~dp0\workers\unreal\Game\Scripts" %SPATIALGDK_SCRIPTSPATH%
-mklink /J "%~dp0\workers\unreal\Game\Binaries\ThirdParty\Improbable\CodeGeneration" %SPATIALGDK_CODEGENPATH%
+mklink /J "%~dp0\workers\unreal\Game\Binaries\ThirdParty\Improbable" %SPATIALGDK_CODEGENPATH%
 
 echo Successfully created symlinks to %SPATIALGDK_PATH%
 
