b97fda57bd13e2557f2e7cbbb59d1920ddd705cc
joshuahuburn <joshuahuburn@improbable.io>
2018-06-20 11:19:26 +0100

    Cleaning script commit

--- safe_clean.bat
+++ safe_clean.bat
@@ -1,17 +1,13 @@
 @ECHO OFF
 
-REM This script is for cleaning your codebase but without damaging your UnrealGDK repository when using the symlink approach.
-REM The reasoning for this extends from git clean removing files located in symlinked directories.
-REM If you are not using the symlinked development approach then the clean stage will also delete your UnrealGDK and this will have to be re-setup.
-
-SET SPATIALGDK_PLUGINSPATH="%~dp0\Game\Plugins\SpatialGDK"
-SET SPATIALGDK_MODULEPATH="%~dp0\Game\Source\SpatialGDK"
-SET SPATIALGDK_SCRIPTSPATH="%~dp0\Game\Scripts"
-SET SPATIALGDK_BINARIESPATH="%~dp0\Game\Binaries\ThirdParty\Improbable"
-SET SPATIALGDK_SCHEMAPATH="%~dp0\spatial\schema\improbable\unreal\gdk"
+set SPATIALGDK_PLUGINSPATH="%~dp0\Game\Plugins\SpatialGDK"
+set SPATIALGDK_MODULEPATH="%~dp0\Game\Source\SpatialGDK"
+set SPATIALGDK_SCRIPTSPATH="%~dp0\Game\Scripts"
+set SPATIALGDK_BINARIESPATH="%~dp0\Game\Binaries\ThirdParty\Improbable"
+set SPATIALGDK_SCHEMAPATH="%~dp0\spatial\schema\improbable\unreal\gdk"
 
 REM First section is for cleaning known UnrealGDK symlinks and running git clean.
-SET /p NUKE=Would you like to remove all UnrealGDK symlinks, then delete uncommited changes and all built files (git clean -xdf)?[Y/N]: 
+set /p NUKE=Would you like to clean all symlinks, uncommited changes and all built files (git clean -xdf)?[Y/N]: 
 IF /I %NUKE% == Y (
     CALL :clean_all_symlinks
     CALL :git_clean_xdf
@@ -28,7 +24,7 @@ IF /I %BUILDBRIDGES% == Y (
 )
 EXIT /B 0
 
-REM Helper methods are below.
+REM All helper methods are below.
 :make_symlinks
 Call create_gdk_symlink.bat %GDKPATH% 
 EXIT /B 0
@@ -54,6 +50,6 @@ EXIT /B 0
 
 :git_clean_xdf
 ECHO Performing git clean
-git clean -xdf
+REM git clean -xdf
 ECHO Finished git clean
 EXIT /B 0
