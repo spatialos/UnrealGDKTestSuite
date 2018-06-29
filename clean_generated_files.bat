fb78bd2108ca9b3c65368a70b8dc2a80d3f6a278
joshuahuburn <joshuahuburn@improbable.io>
2018-06-29 11:49:13 +0100

    Some cleanup and refactoring

--- clean_generated_files.bat
+++ clean_generated_files.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
 
-REM When flexible project structure is implemented we can gather this information from the project structure descriptor file.
-set GENERATED_SCHEMA="%~dp0spatial\schema\improbable\unreal\generated"
-set GENERATED_TYPE_BINDINGS="%~dp0Game\Source\SampleGame\Generated"
-set GENERATED_WORKER_FILES="%~dp0Game\Source\SpatialGDK\Generated"
+:: When flexible project structure is implemented we can gather this information from the project structure descriptor file.
+SET GENERATED_SCHEMA="%~dp0spatial\schema\improbable\unreal\generated"
+SET GENERATED_TYPE_BINDINGS="%~dp0Game\Source\SampleGame\Generated"
+SET GENERATED_WORKER_FILES="%~dp0Game\Source\SpatialGDK\Generated"
 
 :: If '-a' argument is specified, clean all without prompt 
 IF /I "%1"=="-a" (
-   CALL :clean_schema
-   CALL :clean_type_bindings
+   CALL :clean_path %GENERATED_SCHEMA%
+   CALL :clean_path %GENERATED_TYPE_BINDINGS%
    CALL :clean_worker_files
 ) ELSE (
     GOTO main
@@ -19,14 +19,14 @@ EXIT /B 0
 :main
 ECHO This script is intended to clean SampleGame generated files and therefore assumes its project structure.
 
-set /p CLEAN_SCHEMA=Clean generated schema?[Y/N]: 
+SET /p CLEAN_SCHEMA=Clean generated schema?[Y/N]: 
 IF /I %CLEAN_SCHEMA% == Y (
-    CALL :clean_schema
+    CALL :clean_path %GENERATED_SCHEMA%
 )
 
 set /p CLEAN_TYPE_BINDINGS=Clean generated type bindings?[Y/N]: 
 IF /I %CLEAN_TYPE_BINDINGS% == Y (
-    CALL :clean_type_bindings
+    CALL :clean_path %GENERATED_TYPE_BINDINGS%
 )
 
 set /p CLEAN_WORKER_FILES=Clean generated worker code?[Y/N]: 
@@ -35,31 +35,27 @@ IF /I %CLEAN_WORKER_FILES% == Y (
 )
 EXIT /B 0
 
-:clean_schema
-IF EXIST %GENERATED_SCHEMA% (
-    rmdir /s /q %GENERATED_SCHEMA%
-    echo Generated schema have been sucessfully removed.
-) ELSE (
-    echo %GENERATED_SCHEMA% did not exist when performing the clean.
-)
-EXIT /B 0
-
-:clean_type_bindings
-IF EXIST %GENERATED_TYPE_BINDINGS% (
-    rmdir /s /q %GENERATED_TYPE_BINDINGS%
-    echo Generated type bindings have been sucessfully removed.
-) ELSE (
-    echo %GENERATED_TYPE_BINDINGS% did not exist when performing the clean.
-)
-EXIT /B 0
-
 :clean_worker_files
-IF EXIST %GENERATED_WORKER_FILES% (
-    rmdir /s /q %GENERATED_WORKER_FILES%
-    echo Generated worker code has been sucessfully removed.
+CALL :clean_path %GENERATED_WORKER_FILES%
+ECHO After removing generated worker code it is reccomended to run Codegen.bat
+CALL :worker_codegen    
+EXIT /B 0
+
+:clean_path
+IF EXIST "%~1" (
+    RMDIR /s /q "%~1"
+    ECHO The folder "%~1" removed.
 ) ELSE (
-    echo %GENERATED_WORKER_FILES% did not exist when performing the clean.
+    ECHO "%~1" did not exist when performing the clean.
 )
+EXIT /B 0
+
+:worker_codegen
+SET /p WORKER_CODEGEN=Run Codegen.bat?[Y/N]: 
+IF /I %WORKER_CODEGEN% == Y (
+    CALL Game/Scripts/Codegen.bat
+    ECHO Successfully generated worker code.
+) 
 EXIT /B 0
 
 :done
--- safe_clean.bat
+++ safe_clean.bat
@@ -1,13 +1,13 @@
 @ECHO OFF
 
-set SPATIALGDK_PLUGINSPATH="%~dp0\Game\Plugins\SpatialGDK"
-set SPATIALGDK_MODULEPATH="%~dp0\Game\Source\SpatialGDK"
-set SPATIALGDK_SCRIPTSPATH="%~dp0\Game\Scripts"
-set SPATIALGDK_BINARIESPATH="%~dp0\Game\Binaries\ThirdParty\Improbable"
-set SPATIALGDK_SCHEMAPATH="%~dp0\spatial\schema\improbable\unreal\gdk"
+SET SPATIALGDK_PLUGINSPATH="%~dp0\Game\Plugins\SpatialGDK"
+SET SPATIALGDK_MODULEPATH="%~dp0\Game\Source\SpatialGDK"
+SET SPATIALGDK_SCRIPTSPATH="%~dp0\Game\Scripts"
+SET SPATIALGDK_BINARIESPATH="%~dp0\Game\Binaries\ThirdParty\Improbable"
+SET SPATIALGDK_SCHEMAPATH="%~dp0\spatial\schema\improbable\unreal\gdk"
 
-REM First section is for cleaning known UnrealGDK symlinks and running git clean.
-set /p NUKE=Would you like to clean all symlinks, uncommited changes and all built files (git clean -xdf)?[Y/N]: 
+:: First section is for cleaning known UnrealGDK symlinks and running git clean.
+SET /p NUKE=Would you like to clean all symlinks, uncommited changes and all built files (git clean -xdf)?[Y/N]: 
 IF /I %NUKE% == Y (
     CALL :clean_all_symlinks
     CALL :git_clean_xdf
@@ -15,18 +15,18 @@ IF /I %NUKE% == Y (
 )
 EXIT /B 0
 
-REM Second section is for rebuilding these symlinks.
+:: Second section is for rebuilding these symlinks.
 :build_symlinks
-set /p BUILDBRIDGES=Would you like to remake symlinks to your Unreal GDK?[Y/N]: 
+SET /p BUILDBRIDGES=Would you like to remake symlinks to your Unreal GDK?[Y/N]: 
 IF /I %BUILDBRIDGES% == Y (
-    set /p GDKPATH=Please provide the path to your Unreal GDK: 
+    SET /p GDKPATH=Please provide the path to your Unreal GDK: 
     CALL :make_symlinks
 )
 EXIT /B 0
 
-REM All helper methods are below.
+:: All helper methods are below.
 :make_symlinks
-Call create_gdk_symlink.bat %GDKPATH% 
+CALL create_gdk_symlink.bat %GDKPATH% 
 EXIT /B 0
 
 :clean_all_symlinks
@@ -41,15 +41,15 @@ EXIT /B 0
 
 :clean_path
 IF EXIST "%~1" (
-    rmdir /s /q "%~1"
-    echo The symlink "%~1" removed
+    RMDIR /s /q "%~1"
+    ECHO The symlink "%~1" removed
 ) ELSE (
-    echo "%~1" did not exist when performing the clean
+    ECHO "%~1" did not exist when performing the clean
 )
 EXIT /B 0
 
 :git_clean_xdf
 ECHO Performing git clean
-REM git clean -xdf
+git clean -xdf
 ECHO Finished git clean
 EXIT /B 0
