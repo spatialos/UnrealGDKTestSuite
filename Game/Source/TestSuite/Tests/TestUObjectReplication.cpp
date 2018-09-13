bd6e8ba62924870e0b2d97b236cd02d80effcdbb
INTERNAL\danielhall <danielhall@improbable.io>
2018-09-13 10:24:18 +0100

    Updated test suite to build against the linux support branch of the gdk locally

--- Game/Source/TestSuite/Tests/TestUObjectReplication.cpp
+++ Game/Source/TestSuite/Tests/TestUObjectReplication.cpp
@@ -6,7 +6,7 @@
 #include "GameFramework/GameModeBase.h"
 #include "UnrealNetwork.h"
 #include "SpatialNetDriver.h"
-#include "Legacy/EntityRegistry.h"
+#include "Utils/EntityRegistry.h"
 
 void ATestUObjectReplication::Tick(float DeltaTime)
 {
@@ -140,8 +140,8 @@ void ATestUObjectReplication::ValidateRPC_Server(ATestActor*  TestDynamicallyCre
 	//Get the net driver
 	USpatialNetDriver* NetDriver = Cast<USpatialNetDriver>(GetWorld()->GetNetDriver());
 	check(NetDriver);
-	worker::EntityId RPCEntityId = NetDriver->GetEntityRegistry()->GetEntityIdFromActor(TestDynamicallyCreatedActor).ToSpatialEntityId();
-	worker::EntityId ServerEntityId = NetDriver->GetEntityRegistry()->GetEntityIdFromActor(DynamicallyCreatedActor).ToSpatialEntityId();
+	Worker_EntityId RPCEntityId = NetDriver->GetEntityRegistry()->GetEntityIdFromActor(TestDynamicallyCreatedActor);
+	Worker_EntityId ServerEntityId = NetDriver->GetEntityRegistry()->GetEntityIdFromActor(DynamicallyCreatedActor);
 	check(RPCEntityId == ServerEntityId);
 
 	// TODO: UNR-238 Add tests.
