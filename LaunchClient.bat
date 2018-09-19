6328d60c182774e4081ef6afa9af53afa4c8a2d5
Daniel Hall <danielhall@improbable.io>
2018-09-19 10:18:26 +0100

    Updated worker scripts

--- LaunchClient.bat
+++ LaunchClient.bat
@@ -1,3 +1,10 @@
 @echo off
+
+pushd "%~dp0\spatial"
+
+spatial worker build build-config
+
+popd
+
 call "%~dp0ProjectPaths.bat"
 "%UNREAL_HOME%\Engine\Binaries\Win64\UE4Editor.exe" "%~dp0%PROJECT_PATH%\%GAME_NAME%.uproject" -game -log -workerType UnrealClient -stdout -nowrite -unattended -nologtimes -nopause -noin -messaging -NoVerifyGC -windowed -ResX=800 -ResY=600
--- LaunchServer.bat
+++ LaunchServer.bat
@@ -1,3 +1,10 @@
 @echo off
+
+pushd "%~dp0\spatial"
+
+spatial worker build build-config
+
+popd
+
 call "%~dp0ProjectPaths.bat"
 "%UNREAL_HOME%\Engine\Binaries\Win64\UE4Editor.exe" "%~dp0%PROJECT_PATH%\%GAME_NAME%.uproject" ThirdPersonExampleMap -server -log -workerType UnrealWorker -stdout -nowrite -unattended -nologtimes -nopause -noin -messaging -SaveToUserDir -NoVerifyGC -windowed -resX=400 -resY=300
