0eac44660d8cabc18c202efef9a805588db3a982
INTERNAL\danielhall <danielhall@improbable.io>
2018-09-26 14:03:42 +0100

    First successful run

--- ci/prepare_build_environment.sh
+++ ci/prepare_build_environment.sh
@@ -2,37 +2,41 @@
 
 source ci/pinned-tools.sh
 
-if ! isTeamCity ; then
-  echo "This script should only be run on the CI agents."
-  exit 0
-fi
+# if ! isTeamCity ; then
+#   echo "This script should only be run on the CI agents."
+#   exit 0
+# fi
 
-if ! isWindows ; then
-  echo "TestSuite can only be built on Windows."
-  exit 0
-fi
+# if ! isWindows ; then
+#   echo "TestSuite can only be built on Windows."
+#   exit 0
+# fi
 
 if [ -z "${UNREAL_GDK_BRANCH+x}" ]; then
   echo "The UNREAL_GDK_BRANCH variable was not set, using master as default."
-  UNREAL_GDK_BRANCH="master"
+  UNREAL_GDK_BRANCH="feature/dynamic-typebindings"
 fi
 
+UNREAL_GDK_INSTALL_DIRECTORY="Game/Plugins"
+
 markStartOfBlock "Clean gdk build directory"
-  rm -rf build/UnrealGDK/
-  mkdir -p build
+  rm -rf ${UNREAL_GDK_INSTALL_DIRECTORY}
+  mkdir -p ${UNREAL_GDK_INSTALL_DIRECTORY}
 markEndOfBlock "Clean gdk build directory"
 
 markStartOfBlock "Clone the GDK from github"
-  git clone git@github.com:improbable/UnrealGDK.git -b ${UNREAL_GDK_BRANCH} --single-branch "build/UnrealGDK"
+  pushd "${UNREAL_GDK_INSTALL_DIRECTORY}"
+    git clone git@github.com:improbable/UnrealGDK.git -b ${UNREAL_GDK_BRANCH} --single-branch
+  popd
 markEndOfBlock "Clone the GDK from github"
 
 markStartOfBlock "Run the GDK setup script"
-  pushd "build/UnrealGDK"
-    ./BuildGDK.bat
+  pushd "${UNREAL_GDK_INSTALL_DIRECTORY}/UnrealGDK"
+    ./Setup.bat
 
     # The current version of Unreal.
     if [ -z "${UNREAL_HOME+x}" ]; then
-      UNREAL_VERSION="$(cat unreal-engine.version)"
+      UNREAL_VERSION="$(cat SpatialGDK/Extras/unreal-engine.version)"
       export UNREAL_HOME="C:/Unreal/UnrealEngine-${UNREAL_VERSION}"
     fi
 
@@ -44,5 +48,5 @@ markStartOfBlock "Run the GDK setup script"
       export LINUX_MULTIARCH_ROOT="${UNREAL_HOME}/ClangToolchain"
     fi
   popd
-  ./CreateGDKSymlinks.bat "build/UnrealGDK"
+
 markEndOfBlock "Run the GDK setup script"
